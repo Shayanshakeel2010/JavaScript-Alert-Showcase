3d16881078912105016f1f4ff41b60a2fc1b55c3
Shayan Shakeel <shayanshakeel2010@gmail.com>
2026-07-23 00:12:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//====== CHAPTER NO : 01 ======//
+//====== QUESTION NO : 01 ======//
+alert("JavaScript Alert \n \n Error! Please enter your valid password ")
+
+//====== QUESTION NO : 02 ======//
+alert("JavaScript Alert \n \n Welcome to Js Land... \n Happy Codding! ")
+
+//====== QUESTION NO : 03 ======//
+alert(" Welcome to Js Land...  ")
+
+//====== QUESTION NO : 05 ======//
+var Dialogs="Helo... I can run JS through my web browser's console"
+console.log(Dialogs)
+
+//====== QUESTION NO : 05 ======//
+
+alert("Prevent this page from creating additional dialogs.")
+
+
+
+//====== CHAPTER NO : 02 ======//
+//====== QUESTION NO : 01 ======//
+var username=""
+//====== QUESTION NO : 01 ======//
+var myname="Shayan Shakeel"
+alert(myname)
+//====== QUESTION NO : 01 ======//
+var message="Hello World!"
+alert(message)
+//====== QUESTION NO : 01 ======//
+
+var  Bio_Data="Shayan Shakeel \n 16 years old \n Certified Web Appication Development "
+alert( Bio_Data ) 
+
+//====== QUESTION NO : 01 ======//
+var email=" Email : \n khanpurstudio@outlook.com"
+alert(email)
+
+//====== QUESTION NO : 01 ======//
+var book="I am trying to learn from the book A smarter Way \n to learn JS"
+alert(book)
+
+
+
